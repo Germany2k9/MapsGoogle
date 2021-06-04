430824f2374651aa718a9aeacabfd2908bc40ed8
Germany2k9 <germanyp20l@gmail.com>
2021-06-04 20:44:34 +0200

    secundario

--- app/src/main/java/com/germany/mapsgoogle/MapsActivity.java
+++ app/src/main/java/com/germany/mapsgoogle/MapsActivity.java
@@ -1,9 +1,15 @@
 package com.germany.mapsgoogle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -11,8 +17,13 @@ import android.view.MenuItem;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.StreetViewPanoramaOptions;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.SupportStreetViewPanoramaFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.germany.mapsgoogle.databinding.ActivityMapsBinding;
@@ -24,6 +35,8 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
+    private  final String TAG = "MainActivity";
+    private final int REQUEST_LOCATION_PERMISSION = 1;
 
 
     @Override
@@ -34,8 +47,12 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         setContentView(binding.getRoot());
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+//        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+        SupportMapFragment mapFragment = SupportMapFragment.newInstance();
+//                .findFragmentById(R.id.map);
+//        mapFragment.getMapAsync(this);
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.map, mapFragment).commit();
         mapFragment.getMapAsync(this);
     }
     @Override
@@ -68,16 +85,19 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         map.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
             @Override
             public void onMapLongClick(LatLng latLng) {
-                map.addMarker(new MarkerOptions().position(latLng));
+                map.addMarker(new MarkerOptions().icon(BitmapDescriptorFactory.defaultMarker
+                        (BitmapDescriptorFactory.HUE_BLUE)).position(latLng));
                 String snippet = String.format(Locale.getDefault(),
                         "Lat: %1$.5f, Long: %2$.5f",
                         latLng.latitude,
                         latLng.longitude);
 
-                map.addMarker(new MarkerOptions()
+                map.addMarker(new MarkerOptions().icon(BitmapDescriptorFactory.defaultMarker
+                        (BitmapDescriptorFactory.HUE_BLUE))
                         .position(latLng)
                         .title(getString(R.string.dropped_pin))
                         .snippet(snippet));
+
             }
         });
     }
@@ -92,20 +112,38 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        try {
+            // Customize the styling of the base map using a JSON object defined
+            // in a raw resource file.
+            boolean success = googleMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                            this, R.raw.map_style));
+
+            if (!success) {
+                Log.e(TAG, "Style parsing failed.");
+            }
+        } catch (Resources.NotFoundException e) {
+            Log.e(TAG, "Can't find style. Error: ", e);
+        }
         mMap = googleMap;
         float zoom=17;
         // Add a marker in Sydney and move the camera
 //        LatLng sydney = new LatLng(-34, 151);
-        LatLng Washington = new LatLng(38.89454767137102, -77.06539564865797);
-        mMap.addMarker(new MarkerOptions().position(Washington).title("Marker in Washington"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(Washington));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Washington, zoom));
+        LatLng NewYork = new LatLng(40.70267960308327, -74.04628335135746);
+        mMap.addMarker(new MarkerOptions().position(NewYork).title("Marker in NewYork"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(NewYork));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(NewYork, zoom));
+
+        GroundOverlayOptions homeOverlay = new GroundOverlayOptions()
+                    .image(BitmapDescriptorFactory.fromResource(R.drawable.droid))
+                .position(NewYork, 30);
 
         setMapLongClick(mMap);
 
         setPoiClick(mMap);
-
-
+        enableMyLocation();
+        mMap.addGroundOverlay(homeOverlay);
+        setInfoWindowClickToPanorama(mMap);
     }
     private void setPoiClick(final GoogleMap map) {
         map.setOnPoiClickListener(new GoogleMap.OnPoiClickListener() {
@@ -115,9 +153,57 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                         .position(poi.latLng)
                         .title(poi.name));
                 poiMarker.showInfoWindow();
+                poiMarker.setTag("poi");
             }
         });
     }
 
+    private void enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) mMap.setMyLocationEnabled(true);
+        else {
+            ActivityCompat.requestPermissions(this, new String[]
+                            {Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_LOCATION_PERMISSION);
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                            String[] permissions,
+                                            int[] grantResults) {
+        // Check if location permissions are granted and if so enable the
+        // location data layer.
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_LOCATION_PERMISSION:
+                if (grantResults.length > 0
+                        && grantResults[0]
+                        == PackageManager.PERMISSION_GRANTED) {
+                    enableMyLocation();
+                    break;
+                }
+        }
+    }private void setInfoWindowClickToPanorama(GoogleMap map) {
+        map.setOnInfoWindowClickListener(
+                new GoogleMap.OnInfoWindowClickListener() {
+                    @Override
+                    public void onInfoWindowClick(Marker marker) {
+                        if (marker.getTag() == "poi") {
+                            StreetViewPanoramaOptions options =
+                                    new StreetViewPanoramaOptions().position(
+                                            marker.getPosition());
+                            SupportStreetViewPanoramaFragment streetViewFragment
+                                    = SupportStreetViewPanoramaFragment
+                                    .newInstance(options);
+                            getSupportFragmentManager().beginTransaction()
+                                    .replace(R.id.map,
+                                            streetViewFragment)
+                                    .addToBackStack(null).commit();
+                        }
+                    }
+                });
+
+    }
 
 }
